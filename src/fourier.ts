5c71c4c53b7d04f87c124a06d63cdec6cb568e16
Anillc <i@anillc.cn>
2022-11-13 17:32:21 +0800

    add pages

---
+++ src/fourier.ts
@@ -0,0 +1,37 @@
+export interface Complex {
+  real: number
+  imagine: number
+}
+
+export function dft(inputs: Complex[]): Complex[] {
+  const count = inputs.length
+  const results: Complex[] = []
+  for (let k = 0; k < count; k++) {
+    const result: Complex = { real: 0, imagine: 0 }
+    inputs.forEach(({ real, imagine }, i) => {
+      const x = 2 * Math.PI * i * k / count
+      const cosx = Math.cos(x)
+      const sinx = -Math.sin(x)
+      result.real    += cosx * real - sinx * imagine
+      result.imagine += cosx * imagine + sinx * real
+    })
+    results.push(result)
+  }
+  return results
+}
+
+export function restore(inputs: Complex[]) {
+  const count = inputs.length
+  let chain = (time: number): Complex => ({ real: 0, imagine: 0 })
+  inputs.forEach((complex, i) => {
+    const last = chain
+    chain = (time) => {
+      const { real, imagine } = last(time)
+      return {
+        real:    real    + complex.real    * Math.cos(2 * Math.PI * i * time / count) / count,
+        imagine: imagine + complex.imagine * Math.sin(2 * Math.PI * i * time / count) / count,
+      }
+    }
+  })
+  return chain
+}
--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,54 @@
-interface Complex {
-  real: number
-  imagine: number
-}
+import { Complex, dft } from './fourier'
 
-function dft(inputs: Complex[]): Complex[] {
-  const count = inputs.length
-  const results: Complex[] = []
-  for (let k = 0; k < count; k++) {
-    const result: Complex = { real: 0, imagine: 0 }
-    inputs.forEach(({ real, imagine }, i) => {
-      const x = 2 * Math.PI * i * k / count
-      const cosx = Math.cos(x)
-      const sinx = -Math.sin(x)
-      result.real    += cosx * real - sinx * imagine
-      result.imagine += cosx * imagine + sinx * real
-    })
-    results.push(result)
+function func(time: number): Complex {
+  return {
+    real: Math.sin(time) * 10 + time * 10,
+    imagine: Math.cos(time) * 200,
   }
-  return results
 }
 
-function restore(inputs: Complex[]) {
-  const count = inputs.length
-  let chain = (time: number): Complex => ({ real: 0, imagine: 0 })
-  inputs.forEach((complex, i) => {
-    const last = chain
-    chain = (time) => {
-      const { real, imagine } = last(time)
-      return {
-        real:    real    + complex.real    * Math.cos(2 * Math.PI * i * time / count) / count,
-        imagine: imagine + complex.imagine * Math.sin(2 * Math.PI * i * time / count) / count,
-      }
-    }
-  })
-  return chain
-}
+const range = [...new Array(1000).keys()].map(x => (x) / 10)
+const values = range.map(func)
+const fs = dft(values)
+
+const circles: [number, number, number][] = fs.flatMap((f, i) => {
+  const v = 2 * Math.PI * i / fs.length
+  return [
+    [f.real / fs.length, v, -Math.PI / 2],
+    [f.imagine / fs.length, v, 0],
+  ]
+})
+
+circles.sort((x, y) => y[0] - x[0])
+
+const canvas: HTMLCanvasElement = document.querySelector('#main')
+const g = canvas.getContext('2d')
+
+let time = 0
+const points: Complex[] = []
+requestAnimationFrame(function anime() {
+  g.clearRect(0, 0, 1500, 1500)
+  g.strokeStyle = '#000000'
+  if (points.length) {
+    points.reduce((last, p) => {
+      g.beginPath()
+      g.moveTo(last.real, last.imagine)
+      g.lineTo(p.real, p.imagine)
+      g.stroke()
+      return p
+    })
+  }
+  const last = circles.reduce(({ real: x, imagine: y }, [r, v, cos]) => {
+    const angle = cos + v * time
+    const nextX = x + r * Math.cos(angle)
+    const nextY = y + r * Math.sin(angle)
+    g.strokeStyle = '#00000055'
+    g.beginPath()
+    g.arc(x, y, Math.abs(r), 0, 2 * Math.PI)
+    g.stroke()
+    return { real: nextX, imagine: nextY }
+  }, { real: 300, imagine: 500 })
+  points.push(last)
+  time++
+  setTimeout(() => requestAnimationFrame(anime), 10)
+})
